6fbfb2f3c73957700aa0d525560888b4b19a3a03
David Urbanek <durbanek@comcast.net>
2022-04-03 10:59:03 -0600

    Initial commit

---
+++ RatioItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatioMaster
+{
+    public class RatioItem: IComparable<RatioItem>
+    {
+        public int Big { get; set; }
+        public int Small { get; set; }
+        public int Difference { get; set; }
+
+        public RatioItem() { }
+        public RatioItem(int big, int small, int difference)
+        {
+            Big = big;
+            Small = small;
+            Difference = difference;
+        }
+
+        public int CompareTo(RatioItem other)
+        {
+            if (other == null) return -1;
+            if (Difference == other.Difference)
+                if (Big == other.Big)
+                    return Small.CompareTo(other.Small);
+                else
+                    return (Big.CompareTo(other.Big));
+            else
+                return Difference.CompareTo(other.Difference);
+        }
+    }
+}
---
+++ RatioMachine.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RatioMaster
+{
+    public class RatioMachine
+    {
+        public int Big { get; set; }
+        public int Small { get; set; }
+        public List<RatioItem> Candidates { get; set; }
+        public RatioItem BestRatio { get; set; }
+        public int MaxSmallNumber { get; set; }
+
+        public RatioMachine()
+        {
+            Candidates = new List<RatioItem>();
+        }
+
+        public RatioMachine(int big, int small) : this()
+        {
+            if (big < -small)
+                throw new ArgumentException($"{big} is not lager than {small}");
+            Big = big;
+            Small = small;
+            MaxSmallNumber = 3;
+        }
+
+        public RatioItem FindBestRatio()
+        {
+            var retVal = new RatioItem();
+
+            GetNearestTwo(1);
+            GetNearestTwo(2);
+            GetNearestTwo(3);
+
+            Candidates.Sort();
+
+            retVal = Candidates[0];
+
+            return retVal;
+        }
+
+        public void GetNearestTwo(int smallNum)
+        {
+            var calc1 = (Big * smallNum) / Small;
+            if (!IsLegal(calc1, smallNum)) calc1++;
+
+            var compare1 = (calc1 * Small)/smallNum;
+            var diff1 = Math.Abs(Big - compare1);
+
+            int calc2;
+            if(compare1 < Big)
+            {
+                calc2 = calc1+1;
+                if (!IsLegal(calc2, smallNum)) calc2++;
+            }
+            else
+            {
+                calc2 = calc1-1;
+                if (!IsLegal(calc2, smallNum)) calc2--;
+            }
+
+            var compare2 = (calc2 * Small) / smallNum;
+            var diff2 = Math.Abs(Big - compare2);
+
+            Candidates.Add(new RatioItem(calc1, smallNum, diff1));
+            Candidates.Add(new RatioItem(calc2, smallNum, diff2));
+        }
+
+        public bool IsLegal(int test, int smallNum)
+        {
+            if (smallNum == 1) return true;
+            if (test % smallNum != 0) return true;
+            return false;
+        }
+    }
+}
